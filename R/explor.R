c7e2055794344861ad5db8e7cbd78c9055bff001
erickim <ekim339@berkeley.edu>
2017-12-05 11:11:45 -0800

    implemented crossover

--- R/explor.R
+++ R/explor.R
@@ -1,9 +1,30 @@
 source("./R/utils.R")
 
+### EXAMPLES ###
+set.seed(1)
+
 Y <- rnorm(100)
 X <- as.data.frame(matrix(rnorm(100*26), nrow = 100))
 names(X) <- letters
 P <- ncol(X)*2
 
-x <- initialize(Y, X, P)
-xaic <- lapply(x, AIC)
+init_pop <- initialize(Y, X, P)
+crossoverSingle <- crossover(init_pop[[1]]$variables,
+                             init_pop[[2]]$variables,
+                             "single")
+# $child1
+# [1] 0 1 1 1 1 1 0 0 0 1 0 1 1 0 1 0 0 0 1 0 0 1 1 1 0 1
+# $child2
+# [1] 1 1 1 0 1 0 0 1 1 1 0 0 1 0 1 1 1 1 0 1 0 0 0 1 1 0
+crossoverMultiple <- crossover(init_pop[[1]]$variables,
+                               init_pop[[2]]$variables,
+                               "multiple",
+                               3)
+#$child1
+#[1] 1 1 0 1 0 0 1 1 1 0 1 1 0 1 1 1 0 1 0 0 1 1 1 0 1
+#$child2
+#[1] 1 1 1 1 1 0 0 0 1 0 0 1 0 1 0 0 1 0 1 0 0 0 1 1 0
+
+
+# ignore below
+#xaic <- lapply(x, AIC)
--- R/utils.R
+++ R/utils.R
@@ -7,6 +7,10 @@
 library(roxygen2)
 library(docstring)
 
+### The following docstrings don't work with help(...) because we don't have
+### a .Rd file yet, but it should work with ?... to access the docstrings
+### from the global environment.
+
 initialize <- function(Y, X, P) {
   #' Genetic Algorithms Initial Population Creation
   #'
@@ -33,15 +37,59 @@ initialize <- function(Y, X, P) {
   return(init_pop)
 }
 
-crossover <- function(type, parent1, parent2) { 
+crossover <- function(parent1,
+                      parent2,
+                      type = "single",
+                      num_splits = NA) { 
   #' Genetic Algorithms Crossover Operation
   #'
   #' Arguments:
-  #' @param type The type of crossover, either 
+  #' @param type The type of crossover, either "single" or "multiple".
   #' @param parent1 The \code{variables} output from a parent.
   #' @param parent2 The \code{variables} output from another parent.
   #'  
   #' @return A list of vectors of the children to return.
   
   N <- length(parent1)
+  
+  if (type == "single"){
+    split <- sample(N, 1)
+    
+    child1 <- c(parent1[1:split], parent2[(split+1):N])
+    child2 <- c(parent2[1:split], parent1[(split+1):N])
+  } else if (type == "multiple") {
+    if (num_splits < 2 | is.na(num_splits)) num_splits = 2
+    
+    splits <- sort(sample(N, num_splits))
+    parent1Split <- list()
+    parent2Split <- list()
+    
+    # first split
+    parent1Split[[1]] <- parent1[1:splits[1]]
+    parent2Split[[1]] <- parent2[1:splits[1]]
+    # middle splits
+    for (i in 2:num_splits) {
+      parent1Split[[i]] <- parent1[(splits[i - 1] + 1):splits[i]]
+      parent2Split[[i]] <- parent2[(splits[i - 1] + 1):splits[i]]
+    }
+    # last split
+    parent1Split[[num_splits + 1]] <- parent1[(splits[num_splits] + 1):N]
+    parent2Split[[num_splits + 1]] <- parent2[(splits[num_splits] + 1):N]
+    
+    child1 <- c()
+    child2 <- c()
+    
+    for (i in 1:num_splits + 1) {
+      if (i %% 2 == 1) {
+        child1 <- c(child1, parent1Split[[i]])
+        child2 <- c(child2, parent2Split[[i]])
+      } else {
+        child1 <- c(child1, parent2Split[[i]])
+        child2 <- c(child2, parent1Split[[i]])
+      }
+    }
+  }
+  
+  children <- list(child1 = child1, child2 = child2)
+  return(children)
 }
